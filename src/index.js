7fec77a1ae495bc38f5964c648265d3e6ab77c71
Noé_B <noebachofner@lxrin.ch>
2025-10-15 22:55:36 +0200

    Feat: Added a drop down Menu for Mobile and added new text to the start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('Hey there! Welcome to my Website')
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        icon.classList.toggle("open");
+    });
+});
+
+const navbarLinks =
+    document.querySelector('.navbar-links');
+const hamburger =
+    document.querySelector('.icon.nav-icon');
+
+hamburger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.navbar-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('open');
+        navbarLinks.classList.remove('active');
+    });
+});
